019ecf2a5b7d71c8cac1fbca25237e748b853029
sinhaapurva25 <sinhaapurva25@gmail.com>
2024-06-03 23:47:35 +0530

    solved some LCs

--- NeetCode/add-binary.py
+++ NeetCode/add-binary.py
@@ -1,34 +1,17 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        if len(a) > len(b):
-            prefix = (len(a) - len(b)) * "0"
-            b = prefix + b
-        elif len(b) > len(a):
-            prefix = (len(b) - len(a)) * "0"
-            a = prefix + a
         s = 0
         c = 0
-
-        sm = [""]*len(a)
-        for i in range(len(a)-1, -1, -1):
-            x = (int(a[i]), int(b[i]), c)
-            if x in [(0, 0, 0),
-                     (0, 1, 1),
-                     (1, 0, 1),
-                     (1, 1, 0)]:
-                s = 0
-            else:
-                s = 1
-            sm[i] = str(s)
-            if x in [(0, 0, 0),
-                     (0, 0, 1),
-                     (0, 1, 0),
-                     (1, 0, 0)]:
-                c = 0
-            else:
-                c = 1
-        res = str(c)+"".join(sm)
-        return str(int(res))
+        a = int(a)
+        b = int(b)
+        while True:
+            s = a ^ b
+            c = a & b << 1
+            if c == 0:
+                break
+            a = s
+            b = c
+        return s
 
 f = Solution()
 f.addBinary(a="11", b="1")
