7994a4118905192530e151ca96a3395e1c027a82
sinhaapurva25 <sinhaapurva25@gmail.com>
2024-06-05 01:45:34 +0530

    solved some LCs

--- NeetCode/valid-palindrome-ii.py
+++ NeetCode/valid-palindrome-ii.py
@@ -1,20 +1,20 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        # print("***** start *****")
-        l, r = s[:len(s) // 2], s[(len(s) // 2) + 1 if len(s) % 2 == 1 else (len(s) // 2):][::-1]
-        # print(s, l, r)
-        if l == r:
-            return True
-        # print(" 1st check completed ")
-        for i in range(len(s)):
-            s_dash = s[:i] + s[i+1:]
-            l, r = s_dash[:len(s_dash) // 2], s_dash[(len(s_dash) // 2) + 1 if len(s_dash) % 2 == 1 else (len(s_dash) // 2):][::-1]
-            # print(s_dash, l, r)
-            if l == r:
-                return True
-        # print("***** end *****")
-        return False
+        i, j = 0, len(s)-1
+        while i < j:
+            if s[i] != s[j]:
+                return self.method_name(i + 1, j, s) | self.method_name(i, j - 1, s)
+            i += 1
+            j -= 1
+        return True
 
+    def method_name(self, x, y, s):
+        while x < y:
+            if s[x] != s[y]:
+                return False
+            x += 1
+            y -= 1
+        return True
 
 f = Solution()
 print(f.validPalindrome("aba"))
@@ -23,3 +23,11 @@ print(f.validPalindrome("yxaaxy"))
 print(f.validPalindrome("abc"))
 print(f.validPalindrome("acbca"))
 print(f.validPalindrome("deeee"))
+print(f.validPalindrome("cxcaac"))
+
+# https://www.youtube.com/watch?v=wX3-411uJH0
+# https://github.com/nikoo28/java-solutions/blob/master/src/main/java/leetcode/easy/ValidPalindromeII.java
+
+# More challenges
+# 1216. Valid Palindrome III
+# 2330. Valid Palindrome IV
