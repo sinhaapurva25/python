e21db0d177152a20a21e050a7460f0fff6ae5829
sinhaapurva25 <sinhaapurva25@gmail.com>
2024-06-07 23:28:37 +0530

    better solution

--- LeetCode/best-time-to-buy-and-sell-stock.py
+++ LeetCode/best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,18 @@
 class Solution:
     def maxProfit(self, prices: list) -> int:
-        mx = 0
-        for i in range(len(prices)-1, -1, -1):
-            for j in range(i-1, -1, -1):
-                # print(prices[i], prices[j], prices[i]-prices[j])
-                diff = prices[i] - prices[j]
-                if prices[i] - prices[j] > 0 and prices[i] - prices[j] > mx:
-                        mx = prices[i] - prices[j]
-            # print()
+        # mx = 0
+        # for i in range(len(prices)-1, -1, -1):
+        #     for j in range(i-1, -1, -1):
+        #         diff = prices[i] - prices[j]
+        #         if diff > 0 and diff > mx:
+        #             mx = diff
+        # return mx
+        mx, mn = 0, float("inf")
+        for i in range(len(prices)):
+            if prices[i] < mn:
+                mn = prices[i]
+            elif prices[i] - mn > mx:
+                mx = prices[i] - mn
         return mx
 s = Solution()
 print(s.maxProfit(prices = [7,1,5,3,6,4]))
