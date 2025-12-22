f9c2dd5b6431cfd4d8b2635881bf2d51351beaba
sinhaapurva25 <sinhaapurva25@gmail.com>
2025-12-22 20:04:05 +0530

    some practice

--- LeetCode/happy-number.py
+++ LeetCode/happy-number.py
@@ -1,26 +1,21 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        all_sum = list()
-        while True:
-            digits = list()
-            m = n
-            while m!=0:
-                digits.append(m%10)
-                m //= 10
-            s = 0
-            for i in digits:
-                s += i*i
-            n = s
-            # if n<=9 or s in all_sum:
-            if n==1 or s in all_sum:
-                break
-            all_sum.append(s)
-        if n == 1:
-            return True
-        else:
-            return False
+        if n == 0: return False
+        res = False
+        cycle = set()
+        while (n != 1) and (n not in cycle):
+            cycle.add(n)
+            sq = 0
+            nm = n
+            while nm != 0:
+                sq += (nm % 10) * (nm % 10)
+                nm //= 10
+            n = sq
+        if n == 1: return True
+        return res
 
 f = Solution()
 print(f.isHappy(19))
-print(f.isHappy(2))
+print(f.isHappy(7))
 print(f.isHappy(1111111))
+print(f.isHappy(0))
